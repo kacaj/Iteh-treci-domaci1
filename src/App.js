45ebb48ce15e7123ce59270ad0eb2f952b7bcc90
kacaj <katarina.jagodic.best@gmail.com>
2023-04-24 09:53:03 +0200

    root component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import DetaljiValute from './komponente/DetaljiValute';
+import Valute from './komponente/Valute';
+import NavigacioniMeni from './komponente/NavigacioniMeni';
+import Info from './komponente/Info';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [valute, setValute] = useState([]);
+   
+    useEffect(() => {
+        axios.get('https://api.coincap.io/v2/assets').then(res => {
+            console.log(JSON.stringify(res));
+            setValute(res.data.data)
+        });
+    }, []);
+
+    const [valuta, setValuta]=useState(valute[0]);
+    const [valuteZaPrikaz, setValuteZaPrikaz]=useState(valute);
+
+    function pretrazi(e){
+      let pretraga=[];
+      valute.forEach((v)=>{
+          if(v.name.toLowerCase().indexOf(e.target.value.toLowerCase())>-1){
+              pretraga.push(v)
+          }
+      })
+      setValuteZaPrikaz(pretraga);
+  }
+  
+  return(
+    <div>
+      <Router>
+        <NavigacioniMeni/>
+        <Routes>
+          <Route exact path="/" element={<Valute  valute={valuteZaPrikaz}   setValuta={setValuta}  pretrazi={pretrazi}/>} />
+          <Route path="/detalji" element={<DetaljiValute valuta={valuta}/>}/>
+          <Route path="/info" element={<Info/>}/>
+        </Routes>
+      </Router >
     </div>
   );
 }
